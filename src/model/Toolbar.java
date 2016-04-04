74d4b2b1a9bc10da6f710ba6df571995cb008f06
Brittany Paielli <bmpaielli@gmail.com>
2016-04-04 00:36:14 -0700

    Added comments

--- src/model/Toolbar.java
+++ src/model/Toolbar.java
@@ -11,53 +11,118 @@ public class Toolbar {
 	private boolean isItalic = false;
 	private boolean isUnderlined = false;
 
+	/**
+	 * This method sets the font
+	 * 
+	 * @param font
+	 *            font to set to current font
+	 */
 	public void setFont(Font font) {
 		this.f = font;
 	}
 
+	/**
+	 * This method sets the font size
+	 * 
+	 * @param fontSize
+	 *            font size to set to current font size
+	 */
 	public void setFontSize(int fontSize) {
 		this.fontSize = fontSize;
 	}
 
+	/**
+	 * This method sets the color of the text
+	 * 
+	 * @param color
+	 *            color to set to the current text color
+	 */
 	public void setColor(Color color) {
 		this.color = color;
 	}
-	
-	public void setIsBold(boolean b){
+
+	/**
+	 * This method changes the bold boolean
+	 * 
+	 * @param b
+	 *            boolean that tells if text is bold
+	 */
+	public void setIsBold(boolean b) {
 		this.isBold = b;
 	}
-	
-	public void setIsItalic(boolean b){
+
+	/**
+	 * This method changes the Italic boolean
+	 * 
+	 * @param b
+	 *            boolean that tells if text is italic
+	 */
+	public void setIsItalic(boolean b) {
 		this.isItalic = b;
 	}
-	
-	public void setIsUnderlined(boolean b){
+
+	/**
+	 * This method changes the Underline boolean
+	 * 
+	 * @param b
+	 *            boolean that tells if the text is underlined
+	 */
+	public void setIsUnderlined(boolean b) {
 		this.isUnderlined = b;
 	}
-	
+
+	/**
+	 * This method returns the font
+	 * 
+	 * @return the current font
+	 */
 	public Font getFont() {
 		return this.f;
 	}
 
+	/**
+	 * This method returns the font size
+	 * 
+	 * @return the current font size
+	 */
 	public int getFontSize() {
 		return this.fontSize;
 	}
 
+	/**
+	 * This method returns the current color
+	 * 
+	 * @return the current color
+	 */
 	public Color getColor() {
 		return this.color;
 	}
 
-	public boolean isBold(){
+	/**
+	 * This method returns the boolean if the text is bold
+	 * 
+	 * @return boolean if text is bold
+	 */
+	public boolean isBold() {
 		return isBold;
 	}
-	
-	public boolean isItalic(){
+
+	/**
+	 * This method returns the boolean if the text is italic
+	 * 
+	 * @return boolean if text is italic
+	 */
+	public boolean isItalic() {
 		return isItalic;
 	}
-	
-	public boolean isUnderlined(){
+
+	/**
+	 * This method returns the boolean if the text is underlined
+	 * 
+	 * @return boolean if the text is underlined
+	 */
+	public boolean isUnderlined() {
 		return isUnderlined;
 	}
-	
-	
+
 }
