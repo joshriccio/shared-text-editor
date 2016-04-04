cb67b7ff9ada682b158d2be5cafaf0a348a60503
Josh Riccio <jriccio@email.arizona.edu>
2016-04-04 08:46:18 -0700

    added tests to test package

--- src/tests/EditorTests.java
+++ src/tests/EditorTests.java
@@ -1,9 +1,31 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.Test;
+
+import model.*;
+
 public class EditorTests {
 
-	public EditorTests() {
-		// TODO Auto-generated constructor stub
-	}
-
+		@Test
+		public void usertestpass(){
+			User user1 = new User("Josh", "123");
+			assertEquals(user1.getPassword(), "123");
+		}
+		
+		@Test
+		public void usertestname(){
+			User user1 = new User("Josh", "123");
+			assertEquals(user1.getUsername(), "Josh");
+		}
+		
+		@Test
+		public void toolbartestColor(){
+			Toolbar tb = new Toolbar();
+			tb.setColor(Color.RED);
+			assertEquals(tb.getColor(), Color.RED);
+		}
 }
