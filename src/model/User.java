c5ac894f220d485cbbdea3aa40a852e107809f5d
smokinaces11 <cdeeran2@gmail.com>
2016-04-13 11:46:18 -0700

    Refactored server, and added java doc to user

--- src/model/User.java
+++ src/model/User.java
@@ -1,15 +1,10 @@
 package model;
 
-import java.awt.Component;
 import java.io.Serializable;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
-
-import javax.swing.ListModel;
-import javax.swing.table.TableModel;
 
 /**
  * This class is designed to handle user account interactions. A unique
@@ -22,7 +17,6 @@ import javax.swing.table.TableModel;
  */
 @SuppressWarnings("serial")
 public class User implements Serializable {
-
 	private String username;
 	private String password;
 	private  String salt;
@@ -63,38 +57,78 @@ public class User implements Serializable {
 		return password;
 	}
 	
+	/**
+	 * This sets the new user password
+	 * @param password
+	 * 					this new password that will be set for the user
+	 */	
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/**
+	 * This sets the salt value for the user's password
+	 * @param inSalt
+	 * 			the salt value for the user's password
+	 */
 	public void setSalt(String inSalt){
 		this.salt = inSalt;
 	}
 	
+	/**
+	 * Get the salt value for the user's password
+	 * @return the salt value for the user's password
+	 */
 	public String getSalt(){
 		return this.salt;
 	}
-		
+	
+	/**
+	 * Returns a list of the documents owned by the user
+	 * @return a list of the documents ownded by the user
+	 */
 	public List<EditableDocument> getOwnedDocuments(){
 		return this.ownedDocs;
 	}
 	
+	/**
+	 * Make the user the owner of the document
+	 * @param document
+	 * 			the name of the document that the user will own
+	 */
 	public void addAsOwner(EditableDocument document){
 		this.ownedDocs.add(document);
 	}
 	
+	/**
+	 * Change the permissions to where the user does not own the document
+	 * @param document
+	 * 					the document to change the permissions on
+	 */
 	public void removeAsOwner(EditableDocument document){
 		this.ownedDocs.remove(document);
 	}
-	
+	/**
+	 * Get the list of documents that the user can edit
+	 * @return the list of documents the user can edit
+	 */
 	public List<EditableDocument> getEditableDocuments(){
 		return this.editableDocs;
 	}
-	
+	/**
+	 * Give the user permissions to edit the document
+	 * @param document
+	 * 					the document to give editable permissions to.
+	 */
 	public void addAsEditor(EditableDocument document){
 		this.editableDocs.add(document);
 	}
 	
+	/**
+	 * Take away the user's permissions to edit the document
+	 * @param document
+	 * 					the to remove the user's editable permissions from
+	 */
 	public void removeAsEditor(EditableDocument document){
 		this.editableDocs.remove(document);
 	}
