8f2ad0b5b6ea92182b9586bb35a7749d6d6dd0f9
Josh Riccio <jriccio@email.arizona.edu>
2016-04-17 20:45:43 -0700

    Added ability to close tabs with a right click

--- src/view/TabbedPane.java
+++ src/view/TabbedPane.java
@@ -2,24 +2,32 @@ package view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.HashMap;
 
 import javax.swing.BorderFactory;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 
 public class TabbedPane extends JTabbedPane{
 	
 	private HashMap<String, JTextPane> textpanemap;
+	private JPopupMenu menu;
 
 	public TabbedPane(String docName) {
 		textpanemap = new HashMap<>();
 		JTextPane textpane = new JTextPane();
 		textpanemap.put(docName, textpane);
+		this.menu = new JPopupMenu();
+		this.setupMenu();
 		textpane.setPreferredSize(new Dimension(100, 100));
 		textpane.setBackground(Color.WHITE);
 		textpane.setText("");
@@ -30,8 +38,10 @@ public class TabbedPane extends JTabbedPane{
 		this.addMouseListener(new MouseListener(){
 
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
+			public void mouseClicked(MouseEvent e) {
+				if (SwingUtilities.isRightMouseButton(e)) {
+					TabbedPane.this.menu.show(e.getComponent(), e.getX(), e.getY());
+				}
 				
 			}
 
@@ -62,6 +72,21 @@ public class TabbedPane extends JTabbedPane{
 		});
 	}
 	
+	private void setupMenu() {
+		JMenuItem item = new JMenuItem("Close Tab");
+		item.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				TabbedPane.this.remove(TabbedPane.this.getSelectedIndex());
+				
+			}
+			
+		});
+		this.menu.add(item);
+		
+	}
+
 	public void addNewTab(String docName){
 		JTextPane textpane = new JTextPane();
 		textpanemap.put(docName, textpane);
