56c249a3c416d8c79ba11d6cdad0f3b01e4e2876
Josh Riccio <jriccio@email.arizona.edu>
2016-04-17 20:37:19 -0700

    added menu items to UsersPane

--- src/view/TabbedPane.java
+++ src/view/TabbedPane.java
@@ -2,6 +2,8 @@ package view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.HashMap;
 
 import javax.swing.BorderFactory;
@@ -25,6 +27,39 @@ public class TabbedPane extends JTabbedPane{
 		Border borderOutline = BorderFactory.createLineBorder(Color.GRAY);
 		textpane.setBorder(borderOutline);
 		this.addTab(docName, scrollpane);
+		this.addMouseListener(new MouseListener(){
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 	
 	public void addNewTab(String docName){
--- src/view/UsersOnline.java
+++ src/view/UsersOnline.java
@@ -2,36 +2,89 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 
 import network.Request;
 import network.RequestCode;
 
-public class UsersOnline extends JPanel{
-	
+public class UsersOnline extends JPanel {
+
 	private static final long serialVersionUID = 1L;
 	private DefaultListModel<String> listmodel;
 	private JList<String> list;
 	private JScrollPane scrollpane;
 	private ObjectOutputStream oos;
-	
-	public UsersOnline(ObjectOutputStream oos){
+	private JPopupMenu menu;
+
+	public UsersOnline(ObjectOutputStream oos) {
+		this.menu = new JPopupMenu();
+		this.setupMenu();
 		this.oos = oos;
 		listmodel = new DefaultListModel<String>();
 		list = new JList<String>(listmodel);
 		scrollpane = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scrollpane.setPreferredSize(new Dimension(120, 100));
-	    setLayout(new BorderLayout());
-	    this.add(scrollpane, BorderLayout.CENTER);
+		setLayout(new BorderLayout());
+		this.list.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (SwingUtilities.isRightMouseButton(e)) {
+					System.out.println("You clicked on " + list.getSelectedValue());
+					UsersOnline.this.menu.show(e.getComponent(), e.getX(), e.getY());
+				}
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
+		this.add(scrollpane, BorderLayout.CENTER);
 	}
-	
-	public void init(){
+
+	private void setupMenu() {
+		JMenuItem item = new JMenuItem("Add as Editor");
+		this.menu.add(item);
+		item = new JMenuItem("Add as Owner");
+		this.menu.add(item);
+		item = new JMenuItem("Send private message");
+		this.menu.add(item);
+	}
+
+	public void init() {
 		Request getUsers = new Request(RequestCode.GET_USER_LIST);
 		try {
 			oos.writeObject(getUsers);
@@ -39,15 +92,15 @@ public class UsersOnline extends JPanel{
 			e.printStackTrace();
 		}
 	}
-	
-	public void updateUsers(String[] userlist){
+
+	public void updateUsers(String[] userlist) {
 		System.out.println("users added");
-		for(int i=0; i< userlist.length; i++){
-			if(userlist[i].substring(0, 1).equals("-")){
-				if(listmodel.contains(userlist[i].substring(1, userlist[i].length()))){
+		for (int i = 0; i < userlist.length; i++) {
+			if (userlist[i].substring(0, 1).equals("-")) {
+				if (listmodel.contains(userlist[i].substring(1, userlist[i].length()))) {
 					listmodel.removeElement(userlist[i].substring(1, userlist[i].length()));
 				}
-			}else if(!listmodel.contains(userlist[i])){
+			} else if (!listmodel.contains(userlist[i])) {
 				listmodel.addElement(userlist[i]);
 			}
 		}
